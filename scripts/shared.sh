8388a4b88275398b947618b71071c8d925d2f0f2
sndv <sndv@mailbox.org>
2025-01-15 01:36:03 +0200

    Respect configured message display time

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -9,28 +9,10 @@ get_tmux_option() {
 	fi
 }
 
-# Ensures a message is displayed for 5 seconds in tmux prompt
+# Display message in tmux
 display_message() {
 	local message=$1
-
-	# display_duration defaults to 5 seconds, if not passed as an argument
-	if [ "$#" -eq 2 ]; then
-		local display_duration=$2
-	else
-		local display_duration="5000"
-	fi
-
-	# saves user-set 'display-time' option
-	local saved_display_time=$(get_tmux_option "display-time" "750")
-
-	# sets message display time to 5 seconds
-	tmux set-option -gq display-time "$display_duration"
-
-	# displays message
 	tmux display-message "$message"
-
-	# restores original 'display-time' value
-	tmux set-option -gq display-time "$saved_display_time"
 }
 
 # simplest solution, taken from here: http://unix.stackexchange.com/a/81689
